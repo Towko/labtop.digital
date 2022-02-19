bfeaa9c3563868e14243d3b4ff43ddc89c33775b
Towko <antonyo0182@gmail.com>
2022-02-19 17:11:01 +0200

    delay loading fix

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,12 @@
 function onEntry(entry) {
     entry.forEach(change => {
+        if(document.documentElement.clientWidth < 768) {
+            if (change.isIntersecting) {
+                change.target.classList.add('showing');
+            } else {
+                change.target.classList.remove('showing');
+            }
+        }
         if (change.intersectionRatio) {
             change.target.classList.add('showing');
         } else {
